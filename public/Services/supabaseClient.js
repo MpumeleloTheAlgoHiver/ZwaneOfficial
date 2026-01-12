3a4c34f36a2bc4c49f440faa8285fcbf742d9909
MpumeleloTheAlgoHiver <mpumelelo@thealgohive.com>
2026-01-12 08:30:34 +0000

    return keys

--- public/Services/supabaseClient.js
+++ public/Services/supabaseClient.js
@@ -1,9 +1,9 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.0/+esm';
 
-const supabaseUrl = import.meta?.env?.VITE_SUPABASE_URL  ;
-//||"https://jmnjkxfxenrudpvjprcu.supabase.co"
-const supabaseAnonKey = import.meta?.env?.VITE_SUPABASE_ANON_KEY ;
-//|| "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptbmpreGZ4ZW5ydWRwdmpwcmN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxODkzNzUsImV4cCI6MjA4MDc2NTM3NX0.X4ZdxzHF0b9GnHklObpIHqnhWvtKjdZnLoah0EVTvHs"
+const supabaseUrl = import.meta?.env?.VITE_SUPABASE_URL ||"https://jmnjkxfxenrudpvjprcu.supabase.co" ;
+//"
+const supabaseAnonKey = import.meta?.env?.VITE_SUPABASE_ANON_KEY|| "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptbmpreGZ4ZW5ydWRwdmpwcmN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxODkzNzUsImV4cCI6MjA4MDc2NTM3NX0.X4ZdxzHF0b9GnHklObpIHqnhWvtKjdZnLoah0EVTvHs" ;
+//
 // --- Sanity Check ---
 // This check ensures the variables are filled.
 if (!supabaseUrl || !supabaseAnonKey || supabaseUrl.includes('YOUR_SUPABASE_URL')) {
